719173ae905b5aa7a5a690b1a5c55485a1e6a0d3
iycodes <iycodes@outlook.com>
2024-11-09 00:29:38 +0100

    new branch updates

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+start=$(date +"%s")
+
+ssh -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} -i key.txt -t -t -o StrictHostKeyChecking=no << 'ENDSSH'
+docker pull iycodes/transportp:latest
+
+CONTAINER_NAME=transportp
+if [ "$(docker ps -qa -f name=$CONTAINER_NAME)" ]; then
+    if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+        echo "Container is running -> stopping it..."
+        docker stop $CONTAINER_NAME;
+    fi
+fi
+
+docker compose up --detach
+
+exit
+ENDSSH
+
+if [ $? -eq 0 ]; then
+  exit 0
+else
+  exit 1
+fi
+
+end=$(date +"%s")
+
+diff=$(($end - $start))
+
+echo "Deployed in : ${diff}s"
