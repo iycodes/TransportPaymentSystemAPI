8fa075333b15a54c9b7f22d2c498014550f70c8a
iycodes <iycodes@outlook.com>
2024-11-10 12:35:57 +0100

    #2

--- src/main/java/com/mkyong/MainApplication.java
+++ src/main/java/com/mkyong/MainApplication.java
@@ -55,7 +55,7 @@ public class MainApplication {
 
         return args -> {
 
-            System.out.println("Running#1.....");
+            System.out.println("Running#2.....");
             // TransactionEntity mockTx = new TransactionEntity("1-2-17149979931", "Ride
             // Payment", 1, 2,
             // BigDecimal.valueOf(60),
